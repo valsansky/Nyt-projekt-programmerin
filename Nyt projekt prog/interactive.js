cda9b4b4f4a4d160420ef4e6eeaca8ee065223d6
valdemar.skytte <valdemar.skytte@gmail.com>
2025-02-02 13:08:30 +0100

    Sinus kurve virker i display

--- Nyt projekt prog/interactive.js
+++ Nyt projekt prog/interactive.js
@@ -5,17 +5,17 @@ let button2Hover; let button2Active; let button2X; let button2Y;
 let button3Hover; let button3Active; let button3X; let button3Y; 
 let sinusIcon; let sawIcon; let squareIcon;
 //amplitude knobs
-let knobAmplitude1Hover; let knobAmplitude1Value;
-let knobAmplitude2Hover; let knobAmplitude2Value;
-let knobAmplitude3Hover; let knobAmplitude3Value;
+let knobAmplitude1Hover; let knobAmplitude1Value=50; let knobAmplitudeX;
+let knobAmplitude2Hover; let knobAmplitude2Value=50;
+let knobAmplitude3Hover; let knobAmplitude3Value=0;
 //hastighed knobs
-let knobHastighed1Hover; let knobHastighed1Value;
-let knobHastighed2Hover; let knobHastighed2Value;
-let knobHastighed3Hover; let knobHastighed3Value;
+let knobHastighed1Hover; let knobHastighed1Value=10; let knobHastighedX;
+let knobHastighed2Hover; let knobHastighed2Value=10;
+let knobHastighed3Hover; let knobHastighed3Value=10;
 //bølgelængde knobs
-let knobBølgelængde1Hover; let knobBølgelængde1Value;
-let knobBølgelængde2Hover; let knobBølgelængde2Value;
-let knobBølgelængde3Hover; let knobBølgelængde3Value; 
+let knobBølgelængde1Hover; let knobBølgelængde1Value=40; let knobBølgelængdeX;
+let knobBølgelængde2Hover; let knobBølgelængde2Value=40;
+let knobBølgelængde3Hover; let knobBølgelængde3Value=40; 
 
 let startX; let startY;
 let type; let modify;
@@ -23,22 +23,22 @@ let dragging;
 
 function checkHover() {
     //tjek om mussen er over knapper
-    if(mouseX > width/2-350 && mouseX < width/2-350+70) { 
+    if(mouseX > button1X && mouseX < button1X+70) { 
         if(mouseY > button1Y && mouseY < button1Y+70) {button1Hover=true;} else { button1Hover=false;}
         if(mouseY > button2Y && mouseY < button2Y+70) {button2Hover=true;} else { button2Hover=false;}
         if(mouseY > button3Y && mouseY < button3Y+70) {button3Hover=true;} else { button3Hover=false;}
     } else { button1Hover=false; button2Hover=false; button3Hover=false; }
 
     //tjek over knobs
-    if(mouseX > 25+button1X+10+150-35 && mouseX < 25+button1X+10+150+35) {
+    if(mouseX > knobAmplitudeX-35 && mouseX < knobAmplitudeX+35) {
         if(mouseY > button1Y && mouseY < button1Y+70) {knobAmplitude1Hover=true;} else { knobAmplitude1Hover=false;}
         if(mouseY > button2Y && mouseY < button2Y+70) {knobAmplitude2Hover=true;} else { knobAmplitude2Hover=false;}
         if(mouseY > button3Y && mouseY < button3Y+70) {knobAmplitude3Hover=true;} else { knobAmplitude3Hover=false;}
-    } else if(mouseX > 25+button1X+10+300-35 && mouseX < 25+button1X+10+300+35) {
+    } else if(mouseX > knobHastighedX-35 && mouseX < knobHastighedX+35) { 
         if(mouseY > button1Y && mouseY < button1Y+70) {knobHastighed1Hover=true;} else { knobHastighed1Hover=false;}
         if(mouseY > button2Y && mouseY < button2Y+70) {knobHastighed2Hover=true;} else { knobHastighed2Hover=false;}
         if(mouseY > button3Y && mouseY < button3Y+70) {knobHastighed3Hover=true;} else { knobHastighed3Hover=false;}
-    }  else if(mouseX > 25+button1X+10+450-35 && mouseX < 25+button1X+10+450+35) {
+    }  else if(mouseX > knobBølgelængdeX-35 && mouseX < knobBølgelængdeX+35) { 
         if(mouseY > button1Y && mouseY < button1Y+70) {knobBølgelængde1Hover=true;} else { knobBølgelængde1Hover=false;}
         if(mouseY > button2Y && mouseY < button2Y+70) {knobBølgelængde2Hover=true;} else { knobBølgelængde2Hover=false;}
         if(mouseY > button3Y && mouseY < button3Y+70) {knobBølgelængde3Hover=true;} else { knobBølgelængde3Hover=false;}
@@ -63,7 +63,7 @@ function checkHover() {
             if(knobBølgelængde2Hover) {dragging = true; startX = mouseX; startY = mouseY; type = "bølgelængde"; modify = 2; knobBølgelængde2Value=10;} else
             if(knobBølgelængde3Hover) {dragging = true; startX = mouseX; startY = mouseY; type = "bølgelængde"; modify = 3; knobBølgelængde3Value=10;}
         }
-        if(dragging){opdaterKnob(type, modify);}
+        if(dragging){ opdaterKnob(type, modify); }
     } else { dragging = false; }
 }   
 
@@ -72,48 +72,31 @@ function checkHover() {
 //opdatering af knob værdier. type kan være amplitude, hastighed eller bølgelængde
 function opdaterKnob(type, modify) {
     if(type==="amplitude") {
-        if(modify===1) {
-            if(knobAmplitude1Value>0 && knobAmplitude1Value<100) { knobAmplitude1Value=(startY-mouseY); } 
-            else if(knobAmplitude1Value<=0){ knobAmplitude1Value = 0.01; }
-            else if(knobAmplitude1Value>=100){ knobAmplitude1Value = 100; }
-        } else if(modify===2) {
-            if(knobAmplitude2Value>0 && knobAmplitude2Value<100) { knobAmplitude2Value=(startY-mouseY); } 
-            else if(knobAmplitude2Value<=0){ knobAmplitude2Value = 0.01; }
-            else if(knobAmplitude2Value>=100){ knobAmplitude2Value = 100; }
-        } else if(modify===3) {
-            if(knobAmplitude3Value>0 && knobAmplitude3Value<100) { knobAmplitude3Value=(startY-mouseY); } 
-            else if(knobAmplitude3Value<=0){ knobAmplitude3Value = 0.01; }
-            else if(knobAmplitude3Value>=100){ knobAmplitude3Value = 100; }
-        }
+        if(modify===1) { if(knobAmplitude1Value>0 && knobAmplitude1Value<100) { knobAmplitude1Value=(startY-mouseY); }   
+        } else if(modify===2) { if(knobAmplitude2Value>0 && knobAmplitude2Value<100) { knobAmplitude2Value=(startY-mouseY); } 
+        } else if(modify===3) { if(knobAmplitude3Value>0 && knobAmplitude3Value<100) { knobAmplitude3Value=(startY-mouseY); }}
     } else if(type==="hastighed") {
-        if(modify===1) {
-            if(knobHastighed1Value>0 && knobHastighed1Value<100) { knobHastighed1Value=(startY-mouseY); } 
-            else if(knobHastighed1Value<=0){ knobHastighed1Value = 0.01; }
-            else if(knobHastighed1Value>=100){ knobHastighed1Value = 100; }
-        } else if(modify===2) {
-            if(knobHastighed2Value>0 && knobHastighed2Value<100) { knobHastighed2Value=(startY-mouseY); } 
-            else if(knobHastighed2Value<=0){ knobHastighed2Value = 0.01; }
-            else if(knobHastighed2Value>=100){ knobHastighed2Value = 100; }
-        } else if(modify===3) {
-            if(knobHastighed3Value>0 && knobHastighed3Value<100) { knobHastighed3Value=(startY-mouseY); } 
-            else if(knobHastighed3Value<=0){ knobHastighed3Value = 0.01; }
-            else if(knobHastighed3Value>=100){ knobHastighed3Value = 100; }
-        }
+        if(modify===1) { if(knobHastighed1Value>0 && knobHastighed1Value<100) { knobHastighed1Value=(startY-mouseY); } 
+        } else if(modify===2) { if(knobHastighed2Value>0 && knobHastighed2Value<100) { knobHastighed2Value=(startY-mouseY); }  
+        } else if(modify===3) { if(knobHastighed3Value>0 && knobHastighed3Value<100) { knobHastighed3Value=(startY-mouseY); }}
     } else if(type==="bølgelængde") {
-        if(modify===1) {
-            if(knobBølgelængde1Value>0 && knobBølgelængde1Value<100) { knobBølgelængde1Value=(startY-mouseY); } 
-            else if(knobBølgelængde1Value<=0){ knobBølgelængde1Value = 0.01; }
-            else if(knobBølgelængde1Value>=100){ knobBølgelængde1Value = 100; }
-        } else if(modify===2) {
-            if(knobBølgelængde2Value>0 && knobBølgelængde2Value<100) { knobBølgelængde2Value=(startY-mouseY); } 
-            else if(knobBølgelængde2Value<=0){ knobBølgelængde2Value = 0.01; }
-            else if(knobBølgelængde2Value>=100){ knobBølgelængde2Value = 100; }
-        } else if(modify===3) {
-            if(knobBølgelængde3Value>0 && knobBølgelængde3Value<100) { knobBølgelængde3Value=(startY-mouseY); } 
-            else if(knobBølgelængde3Value<=0){ knobBølgelængde3Value = 0.01; }
-            else if(knobBølgelængde3Value>=100){ knobBølgelængde3Value = 100; }
-        }
+        if(modify===1) { if(knobBølgelængde1Value>0 && knobBølgelængde1Value<100) { knobBølgelængde1Value=(startY-mouseY); } 
+        } else if(modify===2) { if(knobBølgelængde2Value>0 && knobBølgelængde2Value<100) { knobBølgelængde2Value=(startY-mouseY); } 
+        } else if(modify===3) { if(knobBølgelængde3Value>0 && knobBølgelængde3Value<100) { knobBølgelængde3Value=(startY-mouseY); }}
     }
+    valueControl();
+}
+
+function valueControl() { //sørger for at værdier aldrig kommer under 0 og over 100.
+    if(knobAmplitude1Value<=0){ knobAmplitude1Value = 0.01; } else if(knobAmplitude1Value>=100){ knobAmplitude1Value = 100; }
+    if(knobAmplitude2Value<=0){ knobAmplitude2Value = 0.01; } else if(knobAmplitude2Value>=100){ knobAmplitude2Value = 100; }
+    if(knobAmplitude3Value<=0){ knobAmplitude3Value = 0.01; } else if(knobAmplitude3Value>=100){ knobAmplitude3Value = 100; }
+    if(knobHastighed1Value<=0){ knobHastighed1Value = 0.01; } else if(knobHastighed1Value>=100){ knobHastighed1Value = 100; }
+    if(knobHastighed2Value<=0){ knobHastighed2Value = 0.01; } else if(knobHastighed2Value>=100){ knobHastighed2Value = 100; }
+    if(knobHastighed3Value<=0){ knobHastighed3Value = 0.01; } else if(knobHastighed3Value>=100){ knobHastighed3Value = 100; }
+    if(knobBølgelængde1Value<=0){ knobBølgelængde1Value = 0.01; } else if(knobBølgelængde1Value>=100){ knobBølgelængde1Value = 100; }
+    if(knobBølgelængde2Value<=0){ knobBølgelængde2Value = 0.01; } else if(knobBølgelængde2Value>=100){ knobBølgelængde2Value = 100; }
+    if(knobBølgelængde3Value<=0){ knobBølgelængde3Value = 0.01; } else if(knobBølgelængde3Value>=100){ knobBølgelængde3Value = 100; }
 }
 
 //mustryk til opdatering af knapper
@@ -125,6 +108,7 @@ function mouseClicked() {
 
 //interaktive elementer tegnes
 function tegnInteraktiv() {
+    strokeWeight(1);
     //tegn knapper
     if(button1Hover && button1Active) { fill(180) } else if(button1Hover || button1Active) { fill(220) } else { fill (255) }
     rect(button1X, button1Y, 70, 70);
@@ -139,31 +123,31 @@ function tegnInteraktiv() {
 
     //tegn knobs:
     //amplitude
-    if(knobAmplitude1Hover) { fill(220) } else { fill (255) }
-    createKnob(25+button1X+10+150, 25+button1Y+10, 70, 1, "amplitude");
-    if(knobAmplitude2Hover) { fill(220) } else { fill (255) }
-    createKnob(25+button2X+10+150, 25+button2Y+10, 70, 2, "amplitude");
-    if(knobAmplitude3Hover) { fill(220) } else { fill (255) }
-    createKnob(25+button3X+10+150, 25+button3Y+10, 70, 3, "amplitude");
+    if(!dragging && knobAmplitude1Hover) { fill(220) } else { fill (255) }
+    createKnob(knobAmplitudeX, 25+button1Y+10, 70, 1, "amplitude");
+    if(!dragging && knobAmplitude2Hover) { fill(220) } else { fill (255) }
+    createKnob(knobAmplitudeX, 25+button2Y+10, 70, 2, "amplitude");
+    if(!dragging && knobAmplitude3Hover) { fill(220) } else { fill (255) }
+    createKnob(knobAmplitudeX, 25+button3Y+10, 70, 3, "amplitude");
     //hastighed
-    if(knobHastighed1Hover) { fill(220) } else { fill (255) }
-    createKnob(25+button1X+10+300, 25+button1Y+10, 70, 1, "hastighed");
-    if(knobHastighed2Hover) { fill(220) } else { fill (255) }
-    createKnob(25+button2X+10+300, 25+button2Y+10, 70, 2, "hastighed");
-    if(knobHastighed3Hover) { fill(220) } else { fill (255) }
-    createKnob(25+button3X+10+300, 25+button3Y+10, 70, 3, "hastighed");
+    if(!dragging && knobHastighed1Hover) { fill(220) } else { fill (255) }
+    createKnob(knobHastighedX, 25+button1Y+10, 70, 1, "hastighed");
+    if(!dragging && knobHastighed2Hover) { fill(220) } else { fill (255) }
+    createKnob(knobHastighedX, 25+button2Y+10, 70, 2, "hastighed");
+    if(!dragging && knobHastighed3Hover) { fill(220) } else { fill (255) }
+    createKnob(knobHastighedX, 25+button3Y+10, 70, 3, "hastighed");
     //bølgelængde
-    if(knobBølgelængde1Hover) { fill(220) } else { fill (255) }
-    createKnob(25+button1X+10+450, 25+button1Y+10, 70, 1, "bølgelængde");
-    if(knobBølgelængde2Hover) { fill(220) } else { fill (255) }
-    createKnob(25+button2X+10+450, 25+button2Y+10, 70, 2, "bølgelængde");
-    if(knobBølgelængde3Hover) { fill(220) } else { fill (255) }
-    createKnob(25+button3X+10+450, 25+button3Y+10, 70, 3, "bølgelængde");
+    if(!dragging && knobBølgelængde1Hover) { fill(220) } else { fill (255) }
+    createKnob(knobBølgelængdeX, 25+button1Y+10, 70, 1, "bølgelængde");
+    if(!dragging && knobBølgelængde2Hover) { fill(220) } else { fill (255) }
+    createKnob(knobBølgelængdeX, 25+button2Y+10, 70, 2, "bølgelængde");
+    if(!dragging && knobBølgelængde3Hover) { fill(220) } else { fill (255) }
+    createKnob(knobBølgelængdeX, 25+button3Y+10, 70, 3, "bølgelængde");
 
     //titel til knobs  
-    fill(0); textSize(20); textAlign(CENTER, CENTER); textFont('Courier New'); text("AMPLITUDE",25+button1X+10+150, 25+button1Y+10-60);
-    fill(0); textSize(20); textAlign(CENTER, CENTER); textFont('Courier New'); text("HASTIGHED",25+button1X+10+300, 25+button1Y+10-60);
-    fill(0); textSize(20); textAlign(CENTER, CENTER); textFont('Courier New'); text("BØLGELÆNGDE",25+button1X+10+450, 25+button1Y+10-60);
+    fill(0); textSize(20); textAlign(CENTER, CENTER); textFont('Courier New'); text("AMPLITUDE",knobAmplitudeX, 25+button1Y+10-60);
+    fill(0); textSize(20); textAlign(CENTER, CENTER); textFont('Courier New'); text("HASTIGHED",knobHastighedX, 25+button1Y+10-60);
+    fill(0); textSize(20); textAlign(CENTER, CENTER); textFont('Courier New'); text("BØLGELÆNGDE",knobBølgelængdeX, 25+button1Y+10-60);
 }
 
 function createKnob(x,y,d,n,t) { 
--- Nyt projekt prog/sketch.js
+++ Nyt projekt prog/sketch.js
@@ -1,3 +1,10 @@
+let inddelingX=10;
+let inddelingY=5;
+let opdelingKurve=100;
+let mainDisplayWidth=700; let mainDisplayheight=300; let fps = 30;
+let punktX; let punktY;
+let tidSinus=0; let tidSaw=0; let tidSquare=0;
+
 function preload() {
     sinusIcon = loadImage("assets/Sinus-wave.png");
     sawIcon = loadImage("assets/Saw-wave.png");
@@ -5,20 +12,107 @@ function preload() {
 }
 
 function setup() {
-	createCanvas(windowWidth, windowHeight); background(240);
+	createCanvas(windowWidth, windowHeight); background(240); frameRate(fps);
     button1X = width/2-350; button1Y = height/3+150+buttonMargin*1;
     button2X = width/2-350; button2Y = height/3+150+buttonMargin*2;
     button3X = width/2-350; button3Y = height/3+150+buttonMargin*3;
-}
 
+    knobAmplitudeX = 25+button1X+10+110;
+    knobHastighedX = 25+button1X+10+315;
+    knobBølgelængdeX = 25+button1X+10+520;
+
+    button1Active = true;
+}
 
 function draw() {
     background(240);
-    fill(255); centerRect(width/2, height/3, 700, 300); //Display
-
+    
     //interaktivitet. se interactive.js
     checkHover();
     tegnInteraktiv();
+
+    //visuelt
+    tegnDisplay();
+    skrivAktuelleVærdier();
+
+    tegnKurve();
+}
+
+function tegnKurve() {
+    tidSinus+=(((mainDisplayWidth-100)/10)/knobBølgelængde1Value/fps)*(knobHastighed1Value/10);
+
+    //tidSaw+=(((1/(opdelingKurve/100))/(knobBølgelængde2Value/10)-floor((1/(opdelingKurve/100))/(knobBølgelængde2Value/10)))/fps*(knobHastighed2Value/10))*(opdelingKurve/100);
+
+    //tidSquare+=(((mainDisplayWidth-100)/10)/knobBølgelængde3Value/fps)*(knobHastighed3Value/10);
+
+    if(button1Active) {
+        for(let i = 0; i < opdelingKurve; i++) {
+            punktX = width/2-(mainDisplayWidth-100)/2+((mainDisplayWidth-100)/opdelingKurve)*i;
+            punktY = height/3+sin(tidSinus+i/(opdelingKurve/100)*(((2*PI)/knobBølgelængde1Value)*(inddelingX/10))) /*amplitude:*/*knobAmplitude1Value*(((mainDisplayheight-50)/2)/100);
+            fill(0); strokeWeight(0.2); circle(punktX,punktY,4);
+        }
+    }
+/*
+    for(let i = 0; i < opdelingKurve; i++) {
+        punktX = width/2-(mainDisplayWidth-100)/2+((mainDisplayWidth-100)/opdelingKurve)*i;
+        punktY = height/3+knobAmplitude2Value*(((mainDisplayheight-50)/2)/100)-((tidSaw+i/(opdelingKurve/100)/(knobBølgelængde2Value)*(inddelingX/10))-floor((tidSaw+i/(opdelingKurve/100)/(knobBølgelængde2Value)*(inddelingX/10))))/*amplitude:/*knobAmplitude2Value*2*(((mainDisplayheight-50)/2)/100);
+        fill(0); strokeWeight(0.2); circle(punktX,punktY,4);
+        
+        strokeWeight(2);
+        
+            line(
+            (knobBølgelængde2Value)*(inddelingX/10),
+            height/3-knobAmplitude2Value*(((mainDisplayheight-50)/2)/100),
+            punktX,
+            height/3+knobAmplitude2Value*(((mainDisplayheight-50)/2)/100));
+        
+    }
+  */  
+    for(let i = 0; i < ((inddelingX*10)/knobBølgelængde2Value); i++) {
+        /*line(width/2-mainDisplayWidth/2+50+i*((mainDisplayWidth-100)/inddelingX)*(knobBølgelængde2Value/10)punktX
+        ,height/3-knobAmplitude2Value*(((mainDisplayheight-50)/2)/100),
+        width/2-mainDisplayWidth/2+50+i*((mainDisplayWidth-100)/inddelingX)*(knobBølgelængde2Value/10),height/3+knobAmplitude2Value*(((mainDisplayheight-50)/2)/100));*/
+    } 
+    
+    //console.log(i*(inddelingX/10))   
+    
+}
+
+function tegnDisplay() {
+    fill(255); strokeWeight(10); centerRect(width/2, height/3, mainDisplayWidth, mainDisplayheight);
+    fill(0); textSize(16); textAlign(CENTER, CENTER); strokeWeight(0.5); text("ALLE MÅL ER I METER", width/2, height/3-180);
+    //x-akse (længde)
+    strokeWeight(2); linjeFraVinkel(width/2-mainDisplayheight,height/3,0,(mainDisplayWidth-100));
+    //y-akse (amplitude)
+    linjeFraVinkel(width/2-mainDisplayheight, height/3-((mainDisplayheight-50)/2), PI/2, mainDisplayheight-50);
+    for(let x = 0; x <= inddelingX; x++) {
+        strokeWeight(0.5); line(width/2-mainDisplayheight+((mainDisplayWidth-100)/inddelingX)*x,height/3-((mainDisplayheight-50)/2),width/2-mainDisplayheight+((mainDisplayWidth-100)/inddelingX)*x,height/3+125);
+        if(x!==0) { stroke(10); text(round(x,2), width/2-mainDisplayheight+((mainDisplayWidth-100)/inddelingX)*x, 12+height/3); }
+    }
+    for(let y = 0; y <= inddelingY; y++) {
+        //over aksen
+        strokeWeight(0.5); line(width/2-mainDisplayheight,height/3-(((mainDisplayheight-50)/2)/inddelingY)*y,width/2+mainDisplayheight,height/3-(((mainDisplayheight-50)/2)/inddelingY)*y);
+        stroke(10); text(round(y/inddelingY,2), -20+width/2-mainDisplayheight, height/3-(((mainDisplayheight-50)/2)/inddelingY)*y);
+        //under aksen:
+        strokeWeight(0.5); line(width/2-mainDisplayheight,height/3+(((mainDisplayheight-50)/2)/inddelingY)*y,width/2+mainDisplayheight,height/3+(((mainDisplayheight-50)/2)/inddelingY)*y);
+        if(y!==0) { stroke(10); text("-"+round(y/inddelingY,2), -20+width/2-mainDisplayheight, height/3+(((mainDisplayheight-50)/2)/inddelingY)*y); }
+    } strokeWeight(1); stroke(1);
+}
+
+function skrivAktuelleVærdier() {
+    textAlign(LEFT, CENTER);
+    text(`A: ${round(knobAmplitude1Value/100,2)} m`,knobAmplitudeX+35+6,button1Y+35);
+    text(`A: ${round(knobAmplitude2Value/100,2)} m`,knobAmplitudeX+35+6,button2Y+35);
+    text(`A: ${round(knobAmplitude3Value/100,2)} m`,knobAmplitudeX+35+6,button3Y+35);
+
+    text(`v: ${round(knobHastighed1Value)/10} m/s`,knobHastighedX+35+6,button1Y+35);
+    text(`v: ${round(knobHastighed2Value)/10} m/s`,knobHastighedX+35+6,button2Y+35);
+    text(`v: ${round(knobHastighed3Value)/10} m/s`,knobHastighedX+35+6,button3Y+35);
+
+    text(`\u03BB: ${round(knobBølgelængde1Value/10,1)} m`,knobBølgelængdeX+35+6,button1Y+35);
+    text(`\u03BB: ${round(knobBølgelængde2Value/10,1)} m`,knobBølgelængdeX+35+6,button2Y+35);
+    text(`\u03BB: ${round(knobBølgelængde3Value/10,1)} m`,knobBølgelængdeX+35+6,button3Y+35);
+
 }
 
 function centerRect(x,y,w,h) {rect(x-w/2, y-h/2, w, h)}
